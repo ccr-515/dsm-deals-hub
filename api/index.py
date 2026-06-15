0ad8800aeae4643d66c8754b05da35ab5681a194
Camilo Rodriguez <515.ux.design@gmail.com>
2026-06-15 13:39:27 -0500

    Use root FastAPI entrypoint on Vercel

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import traceback
-
-try:
-    from app.main import app
-except Exception:
-    print("DSM_DEALS_IMPORT_ERROR_START")
-    traceback.print_exc()
-    print("DSM_DEALS_IMPORT_ERROR_END")
-    raise
