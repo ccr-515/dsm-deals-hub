703dc8feb581a1a3ede41c724faac2fbdce90a59
Camilo Rodriguez <515.ux.design@gmail.com>
2026-06-15 13:31:02 -0500

    Log Vercel FastAPI import errors

--- api/index.py
+++ api/index.py
@@ -1,2 +1,9 @@
-from app.main import app
+import traceback
 
+try:
+    from app.main import app
+except Exception:
+    print("DSM_DEALS_IMPORT_ERROR_START")
+    traceback.print_exc()
+    print("DSM_DEALS_IMPORT_ERROR_END")
+    raise
